11a96de5a4b992cc25c9f407fc244e9a934fe3fe
VolksRat71 <nathanieljryan1994@gmail.com>
2019-06-20 13:48:49 -0700

    pseudo-code update

--- Letter.js
+++ Letter.js
@@ -1,1 +1,9 @@
-// letter.js
+// function to take array item & convert letters to underscores to create underlying charactor
+
+// function to covert letter guessed to boolean "true" or "false"
+
+// function to return whether letter has been guessed or replace underscore with correct underlying letter
+
+// function that takes charactors as arguements & checks against underlying charactor while updating boolean value
+
+//**HINT** this file should not require any other files
--- Word.js
+++ Word.js
@@ -1,1 +1,10 @@
-// word.js
+// function to take letters from uderlying word and make an individual letter array
+
+//// function that returns a string representing the word. This should call the function
+// on each letter object (the first function defined in `Letter.js`) that displays the 
+//character or an underscore and concatenate those together.
+
+////function that takes a character as an argument and calls the guess function on each 
+//letter object (the second function defined in `Letter.js`)
+
+//**Hint** THis file should only require "Letter.js"
