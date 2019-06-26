5fde68f8b6c75e298b4e95a43f2b467779d8f2ef
VolksRat71 <nathanieljryan1994@gmail.com>
2019-06-25 17:34:37 -0700

    working with minor bugs, sometimes doesnt register letters

--- Letter.js
+++ Letter.js
@@ -1,28 +1,25 @@
-// function to take array item & convert letters to underscores to create underlying charactor
+function Letter(value) {
+    this.letter = value;
+    this.guessed = false;
+    this.toString = function () {
 
-// function to covert letter guessed to boolean "true" or "false"
-
-// function to return whether letter has been guessed or replace underscore with correct underlying letter
-
-// function that takes charactors as arguements & checks against underlying charactor while updating boolean value
-
-//**HINT** this file should not require any other files
-
-function Letter (letter) {
-    this.letter = letter;
-    this.guess = false;
-};
-
-Letter.prototype.underlying = function() {
-    if (this.guess === true) {
-        return this.letter
-    } else {return "_"}
-};
-
-Letter.prototype.check = function (guess) {
-    if (this.letter === guess){
-        this.guess = true 
-    } 
-};
+        if (this.letter === " ") {
+            this.guessed = true;
+            return " ";
+        } else {
+            if (this.guessed === false) {
+                return "_";
+            } else {
+                return this.letter;
+            }
+        }
+    };
+    
+    this.guess = function (guess) {
+        if (guess === this.letter) {
+            this.guessed = true;
+        }
+    }
+}
 
 module.exports = Letter;
--- Word.js
+++ Word.js
@@ -1,27 +1,27 @@
-// function to take letters from underlying word and make an individual letter array
+var Letter = require("./letter.js");
 
-//// function that returns a string representing the word. This should call the function
-// on each letter object (the first function defined in `Letter.js`) that displays the 
-//character or an underscore and concatenate those together.
-
-////function that takes a character as an argument and calls the guess function on each 
-//letter object (the second function defined in `Letter.js`)
-
-//**Hint** THis file should only require "Letter.js"
-
-var Letter = require("./Letter.js");
-
-function Word (word){
-    this.word = word;
-    this.wordBreakdown = this.pushArr(word);
-}
-
-Word.prototype.pushArr = function (word){
-    var wordArr = [];
-    for (i = 0; i < word.length; i++){
-        wordArr.push(new Letter(word[i]))
+function Word(answer) {
+    //Letter objects array
+    this.objArray = [];
+    
+    for (var i = 0; i < answer.length; i++) {
+        var letter = new Letter(answer[i]);
+        this.objArray.push(letter);
     }
-    return wordArr;
-};
+    
+    this.log = function () {
+        answerLog = "";
+        for (var i = 0; i < this.objArray.length; i++) {
+            answerLog += this.objArray[i] + " ";
+        }
+        console.log(answerLog + "\n");
+    }
+    
+    this.userGuess = function (input) {
+        for (var i = 0; i < this.objArray.length; i++) {
+            this.objArray[i].guess(input);
+        }
+    }
+}
 
 module.exports = Word;
